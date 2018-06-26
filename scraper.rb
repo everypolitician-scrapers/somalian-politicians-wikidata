9cf087aed31f2777d3f5c6a310167e93f5700442
Tony Bowden <tony@tmtm.com>
2018-06-26 09:02:04 +0100

    Also find via Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,12 @@
 
 require 'wikidata/fetcher'
 
+query = 'SELECT DISTINCT ?item { ?item wdt:P39 wd:Q6589874 }'
+ids = EveryPolitician::Wikidata.sparql(query)
+
 en = WikiData::Category.new('Category:Somalian politicians', 'en').member_titles |
      WikiData::Category.new('Category:Members of the Parliament of Somalia', 'en').member_titles |
      WikiData::Category.new('Category:Somalian politician stubs', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: en })
 
